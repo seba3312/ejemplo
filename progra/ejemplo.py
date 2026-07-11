e2e50bdc972d9a4a5aaf1ca4cfa9f3b8d2a493d2
seba3312 <sebastian.landeros1002@gmail.com>
2026-07-11 17:48:14 -0400

    agregue validadores de numeros y de texto

--- progra/ejemplo.py
+++ progra/ejemplo.py
@@ -1,3 +1,28 @@
+def validar_nombre(mensaje:str)->str:
+    while True:
+        nombre = input(mensaje)
+        if len(nombre) == 0:
+            print("El nombre no puede estar vacio.")
+        elif " " in nombre:
+            print("El nombre no debe contener espacios.")
+        else:
+            return nombre
+def validar_numeros(mensaje:str)->int:
+    while True:
+        try:
+            valor = int(input(mensaje))
+            if valor == 0:
+                print("El valor no puede ser cero.")
+            elif valor < 0:
+                print("No se pueden ingresar numeros negativos.")
+            elif " " in str(valor):
+                print("El valor no debe contener espacios.")
+            else:
+                return valor
+        except Exception:
+            print("Solo se pueden ingresar numeros enteros. ")
+
+
 def menu():
     print("========== MENÚ PRINCIPAL ==========")
     print("1. Stock de Notebook por código")
@@ -7,4 +32,5 @@ def menu():
     print("5. Eliminar Notebook")
     print("6. Salir del programa")
     print("=====================================")
+    opcion = validar_numeros("Seleccione una opción: ")
 menu()
